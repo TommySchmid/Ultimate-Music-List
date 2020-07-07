6f3b2f52d5bdf7519e0e2fdb68d47144cb592213
Tommy Schmid <tommys@s1106.ofc.lair>
2020-07-06 22:03:18 -0400

    Refactored form submit

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -5,20 +5,15 @@ export const FETCH_MUSIC_LIST = 'FETCH_MUSIC_LIST';
 
 export const form_submit = (data) => {
     return function (dispatch) {
-        const post = data;
-        axios.post('/.json', post)
-            .then(setTimeout(() => {
-                axios.get('/.json')
-                .then((response) => dispatch({
-                    type: FETCH_MUSIC_LIST,
-                    data: response.data
-                }))
-                .catch(error => {
-                    console.log('error caught', error);
-                })
-            }, 500))
+        axios.post('/.json', data)
+            .then((response) => {
+                dispatch(fetch_music_list());
+            })
+            .catch(error => {
+                console.log('error caught', error);
+            });
     }
-}
+};
 
 export const fetch_music_list = () => {
     return function (dispatch) {
