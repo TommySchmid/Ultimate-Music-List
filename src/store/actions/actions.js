7b6a4b0c80db5c9410915eb884b5b4dd0c4a961c
Tommy Schmid <tommys@s1149.ofc.lair>
2021-01-27 11:45:20 -0500

    Added ability to delete list item with click. Checkpoint.

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -3,8 +3,10 @@ import axios from '../../hoc/axios';
 export const FORM_SUBMIT = 'FORM_SUBMIT';
 export const DELETE_LIST_ITEM = 'DELETE_LIST_ITEM';
 export const FETCH_MUSIC_LIST = 'FETCH_MUSIC_LIST';
+export const UPDATE_LIST = 'UPDATE_LIST';
 
 export const form_submit = (data) => {
+    console.log(data);
     return function (dispatch) {
         axios.post('/.json', data)
             .then((response) => {
@@ -21,14 +23,25 @@ export const form_submit = (data) => {
 
 export const delete_list_item = (key) => {
     return function (dispatch) {
+        var i = 0;
+        var newList = [];
         axios.get('/.json')
             .then((response) => {
-                console.log(key);
-                Object.values(response.data).forEach((key) => {
-                    if (key === 'Cory Wong'){
-                        alert('jenga');
+                for (i; i < Object.values(response.data).length; i++) {
+                    if (key !== Object.values(response.data)[i].artist) {
+                        newList.push(Object.values(response.data)[i]);
                     }
-                })
+                }
+                axios.delete('/.json')
+                    .then((response) => {
+                        i = 0;
+                        for (i; i < newList.length; i++) {
+                            axios.post('/.json', {artist: newList[i].artist, btnIsDisabled: false})
+                                .then((response) => {
+                                    dispatch(fetch_music_list());
+                                })
+                        }
+                    })
             })
             .catch(error => {
                 console.log('error caught', error);
